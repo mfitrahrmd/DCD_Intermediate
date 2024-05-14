88c5d55804c1e2731c795d6669ab6ddf8dc10b0e
mfitrahrmd <tgcfitrah26@gmail.com>
2024-05-14 21:33:40 +0700

    fix unimplemented

--- my/src/main/java/com/mfitrahrmd/my/EditText.kt
+++ my/src/main/java/com/mfitrahrmd/my/EditText.kt
@@ -21,7 +21,6 @@ class EditText @JvmOverloads constructor(
         setOnTouchListener(this)
         addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
@@ -29,7 +28,6 @@ class EditText @JvmOverloads constructor(
             }
 
             override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
             }
         })
     }
